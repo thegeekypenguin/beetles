eb640ed6f9ee0d899ca105c2943127ae17ba69c9
Shubham Sahay <pihusahay9999@gmail.com>
2023-06-09 01:05:38 +0530

    second push!

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 console.log("Welcome to Podify!")
 // Initialize the variable
 let songIndex= 0;
-let audioElement= new Audio('a_thousand_years.mp3');
+let audioElement= new Audio('songs/1_a_thousand_years.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
-
+let songItems = Array.from(document,getElementsByClassName('songItem'));
 
 
 
 let songs = [
-    {songName: "Jashn-e-Bahara", filePath: "song/1.mp3", coverPath:"covers/1.jpeg"},
-    {songName: "Jashn-e-Bahara", filePath: "song/1.mp3", coverPath:"covers/1.jpeg"},
-    {songName: "Jashn-e-Bahara", filePath: "song/1.mp3", coverPath:"covers/1.jpeg"},
-    {songName: "Jashn-e-Bahara", filePath: "song/1.mp3", coverPath:"covers/1.jpeg"},
-    {songName: "Jashn-e-Bahara", filePath: "song/1.mp3", coverPath:"covers/1.jpeg"}
+    {songName: "a_thousand_years", filePath: "songs/1_a_thousand_years.mp3", coverPath:"covers/1.jpg"},
+    {songName: "man_on_the_moon", filePath: "songs/2_man_on_the_moon.mp3", coverPath:"covers/2.jpg"},
+    {songName: "world_we_used_to_know", filePath: "songs/3_world_we_used_to_know.mp3", coverPath:"covers/3.jpg"},
+    {songName: "space_melody", filePath: "songs/4_space_melody.mp3", coverPath:"covers/4.jpg"},
+    {songName: "catch_me_if_you_can", filePath: "songs/5_catch_me_if_you_can.mp3", coverPath:"covers/5.jpg"}
 ]
  
+songItems.forEach((element, i)=> {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
 
 // audioElement.play();
 
@@ -50,3 +54,4 @@ audioElement.addEventListener('timeupdate', ()=>{
  myProgressBar.addEventListener('change', ()=>{
     audioElement.currentTime= myProgressBar.value * audioElement.duration/100;
  })
+
